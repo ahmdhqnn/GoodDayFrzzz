01897258e62634673acedfb50fac7294ba68a45d
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-05-13 20:52:34 +0700

    fix bug

--- app/src/main/java/org/ahmad/project1app/ui/screen/ModuleViewModel.kt
+++ app/src/main/java/org/ahmad/project1app/ui/screen/ModuleViewModel.kt
@@ -28,62 +28,54 @@ class ModuleViewModel : ViewModel() {
         Module(
             2,
             "Modul 2. Mekanisme Pernapasan",
-            "1. Hidung\n" +
-                    "Proses bernapas pada manusia diawali dengan menghirup udara melalui lubang hidung ke dalam rongga hidung. Di dalam rongga hidung, udara mengalami penyaringan oleh rambut-rambut halus sehingga kotoran dan debu tidak ikut masuk bersama udara. Selain disaring, udara yang masuk dilembapkan dan mengalami penyesuaian suhu oleh selaput\n" +
-                    "hidung.\n" +
-                    "2. Faring\n" +
-                    "Proses setelah udara masuk ke hidung adalah masuk ke faring. Pada faring terdapat episglotis yang berfungsi sebagai pembatas dantara saluran pernapasan dengan saluran pencernaan. Faring berfungsi untuk menghubungkan rongga hidung dengan laring.\n" +
-                    "3. Laring\n" +
-                    "Proses setelah udara masuk ke faring adalah masuk ke laring. Laring atau tekak (jakun) terdapat di bagian belakang faring. Laring terdiri atas sembilan susunan tulang rawan berbentuk kotak. Laring berfungsi sebagai tempat melekatnya selaput atau pita suara. Aliran udara akan menggetarkan pita suara sehingga menghasilkan suara. Pada laring terdapat katup epiglottis yang otomatis tertutup saat menelan makanan sehingga tidak masuk ke saluran pernapasan.\n" +
-                    "4. Trakea\n" +
-                    "Trakea terletak di depan kerongkongan dan tersusun atas tulang-tulang rawan berbentuk cincin. Dinding trakea dilapisi oleh sel epitel berambut getar (silia) dan selaput lendir. Keduanya berfungsi menahan dan mengeluarkan kotoran-kotoran yang terbawa masuk bersama udara. Trakea bercabang dua, yang satu menuju paru-paru kiri dan yang satu menuju paru-paru kanan.\n" +
-                    "5. Bronkus\t\n" +
-                    "batang tenggorokan bercabang dua yang menuju paru-paru kanan dan kiri. Pada persimpangan tersebut terdapat sensor batuk.Jika ada nasi atau benda asing pada saluran pernapasan maka benda itu akan didorong ke atas dengan cara batuk. Itu sebabnya jika tersedak, secara spontan kalian akan batuk. Hal ini untuk menghindari masuknya benda-benda asing ke dalam paru-paru.\n" +
-                    " Setiap bronkus bercabang lagi menjadi bronkiolus. Cabang bronkiolus akan masuk ke dalam gelembung paru-paru yang disebut alveolus.\n" +
-                    "6. Paru-paru\n" +
-                    "Semua udara yang kita hirup akan masuk ke dalam paru-paru. Tanpa paru-paru, kita tidak mungkin bisa bernapas. Paru-paru terletak di rongga dada tubuh. Manusia mempunyai dua paru-paru, yaitu paru-paru bagian kanan dan kiri. Paru-paru terdiri atas satuan kecil yang dinamakan alveolus. Dalam alveolus, akan terjadi pertukaran udara antara oksigen yang kita hirup dengan karbon dioksida. Oksigen akan diedarkan ke seluruh tubuh. Sementara karbon dioksida dikeluarkan lewat hembusan napas.\n" +
-                    "7. Diafragma\n" +
-                    "Antara rongga dada dan rongga perut kita terdapat sekat pembatas berupa otot yang disebut diafragma. Ketika menarik napas, diafragma akan menekan ke bagian bawah tubuh sehingga paru-paru mengembang dan udara dari luar bisa masuk. Ketika kita membuang napas maka diafragma akan melengkung dan menekan ke bagian atas tubuh sehingga udara di dalam paru-paru keluar."
+            "-\tTahap 1: Menarik napas\n" +
+                    "Untuk menarik udara dari luar atau oksigen, maka tahap pertama yang kita lakukan adalah menarik napas. Saat kita menarik napas, diafragma akan turun dan membuat ruang di dalam dada kita menjadi lebih besar. Karena ruang di dalam dada kita membesar, udara dari luar akan masuk melalui hidung atau mulut kita. Udara ini akan melewati tenggorokan, lalu masuk ke paru-paru. Di sana, udara akan bercabang seperti pohon. Cabang-cabang ini semakin kecil dan akhirnya berakhir di tempat yang sangat kecil bernama alveolus. Di dalam alveolus inilah oksigen yang kita hirup akan berpindah dari udara ke dalam darah. Darah kita kemudian akan membawa oksigen ini ke seluruh tubuh kita, termasuk ke otak, jantung, dan otot.\n" +
+                    "-\tTahap 2: Pertukaran oksigen dan karbon dioksida\n" +
+                    "Bayangkan alveolus seperti pasar. Di pasar ini, ada dua jenis barang yang ditukar, yakni oksigen dan karbon dioksida. Oksigen adalah barang yang kita butuhkan sedangkan karbon dioksida adalah barang yang harus dibuang. Prosesnya diawali dengan darah yang membawa oksigen ke seluruh sel-sel tubuh untuk menghasilkan energi. Sel tubuh menghasilkan zat sisa berupa karbon dioksida. Nah, karbon dioksida ini kemudian akan kembali ke darah.\n" +
+                    "-\tTahap 3: Membuang napas\n" +
+                    "Setelah karbon dioksida masuk ke paru-paru, kita perlu membuangnya. Caranya dengan menghembuskan napas. Saat kita menghembuskan napas, diafragma akan naik kembali. Jadi, ruang di dada akan jadi lebih kecil. Karena ruang dalam dada jadi lebih kecil, udara kotor akan terdorong melalui hidung atau mulut kita.\n"
         ),
         Module(
             3,
             "Modul 3: Macam-macam cara bernapas",
-            "1. Hidung\n" +
-                    "Proses bernapas pada manusia diawali dengan menghirup udara melalui lubang hidung ke dalam rongga hidung. Di dalam rongga hidung, udara mengalami penyaringan oleh rambut-rambut halus sehingga kotoran dan debu tidak ikut masuk bersama udara. Selain disaring, udara yang masuk dilembapkan dan mengalami penyesuaian suhu oleh selaput\n" +
-                    "hidung.\n" +
-                    "2. Faring\n" +
-                    "Proses setelah udara masuk ke hidung adalah masuk ke faring. Pada faring terdapat episglotis yang berfungsi sebagai pembatas dantara saluran pernapasan dengan saluran pencernaan. Faring berfungsi untuk menghubungkan rongga hidung dengan laring.\n" +
-                    "3. Laring\n" +
-                    "Proses setelah udara masuk ke faring adalah masuk ke laring. Laring atau tekak (jakun) terdapat di bagian belakang faring. Laring terdiri atas sembilan susunan tulang rawan berbentuk kotak. Laring berfungsi sebagai tempat melekatnya selaput atau pita suara. Aliran udara akan menggetarkan pita suara sehingga menghasilkan suara. Pada laring terdapat katup epiglottis yang otomatis tertutup saat menelan makanan sehingga tidak masuk ke saluran pernapasan.\n" +
-                    "4. Trakea\n" +
-                    "Trakea terletak di depan kerongkongan dan tersusun atas tulang-tulang rawan berbentuk cincin. Dinding trakea dilapisi oleh sel epitel berambut getar (silia) dan selaput lendir. Keduanya berfungsi menahan dan mengeluarkan kotoran-kotoran yang terbawa masuk bersama udara. Trakea bercabang dua, yang satu menuju paru-paru kiri dan yang satu menuju paru-paru kanan.\n" +
-                    "5. Bronkus\t\n" +
-                    "batang tenggorokan bercabang dua yang menuju paru-paru kanan dan kiri. Pada persimpangan tersebut terdapat sensor batuk.Jika ada nasi atau benda asing pada saluran pernapasan maka benda itu akan didorong ke atas dengan cara batuk. Itu sebabnya jika tersedak, secara spontan kalian akan batuk. Hal ini untuk menghindari masuknya benda-benda asing ke dalam paru-paru.\n" +
-                    " Setiap bronkus bercabang lagi menjadi bronkiolus. Cabang bronkiolus akan masuk ke dalam gelembung paru-paru yang disebut alveolus.\n" +
-                    "6. Paru-paru\n" +
-                    "Semua udara yang kita hirup akan masuk ke dalam paru-paru. Tanpa paru-paru, kita tidak mungkin bisa bernapas. Paru-paru terletak di rongga dada tubuh. Manusia mempunyai dua paru-paru, yaitu paru-paru bagian kanan dan kiri. Paru-paru terdiri atas satuan kecil yang dinamakan alveolus. Dalam alveolus, akan terjadi pertukaran udara antara oksigen yang kita hirup dengan karbon dioksida. Oksigen akan diedarkan ke seluruh tubuh. Sementara karbon dioksida dikeluarkan lewat hembusan napas.\n" +
-                    "7. Diafragma\n" +
-                    "Antara rongga dada dan rongga perut kita terdapat sekat pembatas berupa otot yang disebut diafragma. Ketika menarik napas, diafragma akan menekan ke bagian bawah tubuh sehingga paru-paru mengembang dan udara dari luar bisa masuk. Ketika kita membuang napas maka diafragma akan melengkung dan menekan ke bagian atas tubuh sehingga udara di dalam paru-paru keluar."
+            "-Pernapasan Dada\n" +
+                    "Pernapasan dada adalah pernapasan yang melibatkan kerja otot antar tulang rusuk. Ada dua tahap dalam pernapasan dada, yaitu saat udara masuk /inspirasi dan saat udara keluar/ekspirasi. Berikut ini adalah penjelasan proses pernapasan dada.\n" +
+                    "-\tUdara masuk/ Inspirasi\n" +
+                    "Saat menghirup udara, otot antar tulang rusuk memanjang, tulang rusuk merenggang dan rongga dada membesar. Akibatnya, tekanan udara dalam paruparu lebih kecil daripada tekanan udara di luar tubuh sehingga udara mengalir masuk ke paru-paru.\n" +
+                    "-\tUdara keluar/ Ekspirasi\n" +
+                    "Saat menghembuskan udara, otot antar tulang rusuk memendek, tulang rusuk merapat, dan rongga dada mengecil. Akibatnya, tekanan udara dalam paru-paru lebih besar daripada tekanan udara di luar tubuh sehingga udara udara mengalir keluar dari paru-paru.\n" +
+                    "\tPernapasan Perut\n" +
+                    "\tPernapasan perut adalah pernapasan yang melibatkan kerja otot diafragma. Diafragma adalah sekat atau pembatas yang menghubungkan rongga dada dengan rongga perut. Diafragma terdiri dari oto-otot yang bergerak naik dan turun.\n" +
+                    "-\tUdara masuk/ Inspirasi\n" +
+                    "Saat menghirup udara, otor diagragma turun (berkontraksi), rongga dada mengembang. Akibatnya tekanan udara di dalam paru-paru lebih kecil daripada tekanan udara di luar tubuh sehingga udara luar mengalir masuk ke paru-paru.\n" +
+                    "-\tUdara keluar/ Ekspirasi\n" +
+                    "Saat menghembuskan udara, otot diafragma melengkung ke atas (kembali ke posisi semula), rongga dada mengecil. Akibatnya, tekanan udara dalam paru-paru lebih besar daripada tekanan udara di luar tubuh sehingga udara mengalir keluar tubuh.\n"
         ),
         Module(
             4,
             "Modul 1: Bagian Sistem Pernapasan",
-            "1. Hidung\n" +
-                    "Proses bernapas pada manusia diawali dengan menghirup udara melalui lubang hidung ke dalam rongga hidung. Di dalam rongga hidung, udara mengalami penyaringan oleh rambut-rambut halus sehingga kotoran dan debu tidak ikut masuk bersama udara. Selain disaring, udara yang masuk dilembapkan dan mengalami penyesuaian suhu oleh selaput\n" +
-                    "hidung.\n" +
-                    "2. Faring\n" +
-                    "Proses setelah udara masuk ke hidung adalah masuk ke faring. Pada faring terdapat episglotis yang berfungsi sebagai pembatas dantara saluran pernapasan dengan saluran pencernaan. Faring berfungsi untuk menghubungkan rongga hidung dengan laring.\n" +
-                    "3. Laring\n" +
-                    "Proses setelah udara masuk ke faring adalah masuk ke laring. Laring atau tekak (jakun) terdapat di bagian belakang faring. Laring terdiri atas sembilan susunan tulang rawan berbentuk kotak. Laring berfungsi sebagai tempat melekatnya selaput atau pita suara. Aliran udara akan menggetarkan pita suara sehingga menghasilkan suara. Pada laring terdapat katup epiglottis yang otomatis tertutup saat menelan makanan sehingga tidak masuk ke saluran pernapasan.\n" +
-                    "4. Trakea\n" +
-                    "Trakea terletak di depan kerongkongan dan tersusun atas tulang-tulang rawan berbentuk cincin. Dinding trakea dilapisi oleh sel epitel berambut getar (silia) dan selaput lendir. Keduanya berfungsi menahan dan mengeluarkan kotoran-kotoran yang terbawa masuk bersama udara. Trakea bercabang dua, yang satu menuju paru-paru kiri dan yang satu menuju paru-paru kanan.\n" +
-                    "5. Bronkus\t\n" +
-                    "batang tenggorokan bercabang dua yang menuju paru-paru kanan dan kiri. Pada persimpangan tersebut terdapat sensor batuk.Jika ada nasi atau benda asing pada saluran pernapasan maka benda itu akan didorong ke atas dengan cara batuk. Itu sebabnya jika tersedak, secara spontan kalian akan batuk. Hal ini untuk menghindari masuknya benda-benda asing ke dalam paru-paru.\n" +
-                    " Setiap bronkus bercabang lagi menjadi bronkiolus. Cabang bronkiolus akan masuk ke dalam gelembung paru-paru yang disebut alveolus.\n" +
-                    "6. Paru-paru\n" +
-                    "Semua udara yang kita hirup akan masuk ke dalam paru-paru. Tanpa paru-paru, kita tidak mungkin bisa bernapas. Paru-paru terletak di rongga dada tubuh. Manusia mempunyai dua paru-paru, yaitu paru-paru bagian kanan dan kiri. Paru-paru terdiri atas satuan kecil yang dinamakan alveolus. Dalam alveolus, akan terjadi pertukaran udara antara oksigen yang kita hirup dengan karbon dioksida. Oksigen akan diedarkan ke seluruh tubuh. Sementara karbon dioksida dikeluarkan lewat hembusan napas.\n" +
-                    "7. Diafragma\n" +
-                    "Antara rongga dada dan rongga perut kita terdapat sekat pembatas berupa otot yang disebut diafragma. Ketika menarik napas, diafragma akan menekan ke bagian bawah tubuh sehingga paru-paru mengembang dan udara dari luar bisa masuk. Ketika kita membuang napas maka diafragma akan melengkung dan menekan ke bagian atas tubuh sehingga udara di dalam paru-paru keluar."
+            "-\tTBC (Tuberculosis)\n" +
+                    "Tuberkulosis atau yang biasa disebut TBC adalah penyakit paru-paru yang disebabkan bakteri Mycobacterium tuberculosis. Bakteri ini tidak hanya menyerang paru-paru, tapi juga bisa menyebar ke bagian tubuh yang lain, seperti tulang, kelenjar getah bening, sistem saraf pusat, dan ginjal. Bakteri menyebar di udara melalui percikan dahak atau cairan dari saluran pernapasan penderita, misalnya saat batuk atau bersin. Jadi, kita perlu berhati-hati agar tidak tertular penyakit ini dari orang lain yang menderita TBC.\n" +
+                    "-\tISPA (Infeksi Saluran Pernapasan Akut)\n" +
+                    "-\tASMA\n" +
+                    "Asma merupakan salah satu penyakit yang menyerang sistem pernapasan. Penyakit ini disebabkan oleh penyempitan saluran pernapasan. Penyempitan ini biasanya disebabkan oleh debu, menghirup udaara tercemar, udara dingin, alergi terhadap makanan tertentu, atau kelelahan. Gejala asma yaitu dada terasa nyeri, sesak, batuk, dan napas terasa berat terutama pada malam hari. Pengobatan asma dapat dilakukan dengan memberikan inhaler. Inhaler berfungsi untuk obat ke dalam saluran pernapasan secara langsung dengan cara dihirup melalui mulut.\n" +
+                    "-\tInfluenza\n" +
+                    "Penyakit influenza disebabkan oleh virus dan mudah sekali menular. Penularan bisa melalui kontak langsung atau melalui cairan yang keluar dari penderita saat batuk atau bersin. Saat flu, hidung kita dipenuhi lendir sehingga mengganggu pernapasan.\n" +
+                    "-\tFLU BURUNG\n" +
+                    "Flu burung merupakan penyakit yang disebabkan oleh virus H5N1. Virus H5N1 ini berkembangbiak dan menyerang unggas, namun virus ini juga dapat menyerang manusia. Flu burung merupakan penyakit menular. Ciri-ciri penyakit flu burung antara lain demam tinggi di atas 39\uD835\uDC42C, sakit kepala dan tenggorokan, serta batuk yang terus menerus setelah kontak dengan unggas. Penyakit ini sangat berbahaya dan dapat menyebabkan kematian. Salah satu upaya untuk mencegah flu burung, yaitu terapkan kebiasaan mencuci tangan dengan sabun terutama setelah melakukan kontak dengan unggas.\n" +
+                    "-\tBRONKITIS\n" +
+                    "Bronkitis adalah peradangan yang terjadi pada bronkus (saluran udara dari dan ke paru-paru). Pada umumnya, bronkitis dicirikan dengan batuk berdahak yang kadang dahaknya bisa berubah warna.\n" +
+                    "-\tSINUSITIS \n" +
+                    "Sinusitis merupakan penyakit yang menyerang organ pernapasan. Sinusitis adalah penyakit peradangan yang menyerang bagian atas rongga hidung atau sinus paranasalis. Penyakit sinusitis disebabkan oleh infeksi bakteri, jamur, virus, menurunnya kekebalan tubuh, flu, stress, kecanduan rokok, dan infeksi pada gigi. Gejala yang terjadi adalah hidung tersumbat, sering bersin, tercium bau tidak sedap, hidung mengeluarkan ingus kental berwarna putih atau kekuning-kungingan dan kepala sakit seperti ada yang menekan. Penyakit sinusitis dapat dicegah dengan cara selalu menjaga daya tahan tubuh, menghilangkan kebiasaan merokok, dan memperbanyak mengonsumsi buah-buahan.\n" +
+
+                    "-\tPNEUMONIA\n" +
+                    "Pneumonia merupakan penyakit radang paru-paru yang disebabkan oleh bakteri Diplococcus Pnemoniae. Alveolus pada penderita penyakit pneumonia mengalami peradangan sehingga dipenuhi oleh nanah dan lendir yang mengakibatkan oksigen sulit berdifusi mencapai darah. Pneumonia juda dapat disebabkan oleh iritasi kimia atau fisik dari paru-paru atau sebagai akiat dari penyakit lainnya, seperti kanker paru-paru atau terlalu banyak minum alkohol. Namun, penyebab yang paling sering ilah serangan bakteri streptococcus pneumoniae atau pneumokokus.\n" +
+                    "-\tEMFISEMA\n" +
+                    "Emfisema adalah penyakit kronis yang terjadi di bagian alveolus atau kantong udara di paru-paru. Penyakit ini ditandai dengan pembengkakan paru-paru karena pembuluh darahnya kemasukan udara. Emfisema menyebabkan hilangnya elastisitas alveolus sehingga membuat penderita sulit bernapas. Penderita mengalami batu kronis dan sesak napas. Asap rokok dan kekurangan enzim alfa-1-antitripsin adalah penyebab hilangnya elastisitas pada paru-paru.\n" +
+                    "-\tKANKER PARU-PARU\n" +
+                    "Kanker paru-paru merupakan penyakit yang menyebabkan terganggunya fungsi paru-paru. Salah satu pemicu penyakit ini adalah kebiasaan merokok. Kanker paru-paru merupakan salah satu penyakit berbahaya yang dapat menyebabkan kematian. Kebanyakan kasus dari kanker paru disebabkan karena rokok. Rokok adalah faktor risiko terbesar dari kanker paru. Rokok mengandung 60 zat racun yang berbeda sehingga dapat menyebabkan kanker. Zat racun tersebut dikenal sebagai zat karsinogenik.\n" +
+                    "-\tFARINGITIS\n" +
+                    "Faringitis adalah radang pada faring akibat infeksi sehingga timbul rasa nyeri pada waktu menelan makanan dan kerongkongan terasa kering. Gangguan ini disebabkan oleh infeksi bakteri atau virus. Bakteri yang biasa menyerang penyakit ini adalah streptococcus pharyngitis. Peradangan juda dapat terjadi karena kebiasaan merokok. Faringitis ditandai dengan rasa sakit saat menelan dan rasa kering di kerongkongan\n"
         ),
 
     )
