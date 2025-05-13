ab85426a9739b3e54515065b0e78c134cb3469f3
Fakhri Abdillah <fakhriabdull03@gmail.com>
2025-05-13 17:54:49 +0700

    Membuat tampilan list

--- app/src/main/java/org/ahmad/project1app/ui/screen/ModuleScreen.kt
+++ app/src/main/java/org/ahmad/project1app/ui/screen/ModuleScreen.kt
@@ -1,25 +1,24 @@
 package org.ahmad.project1app.ui.screen
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
 import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -29,8 +28,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -107,16 +104,25 @@ private fun ScreenContent(modifier: Modifier) {
     LazyColumn(modifier = modifier.fillMaxSize()) {
         items(data) {
             ListItem(module = it)
+
         }
     }
 }
 
 @Composable
 fun ListItem(module: Module) {
-    Column {
-        Text(text = module.title, fontWeight = FontWeight.Bold)
-        Text(text = module.content)
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        modifier = Modifier.padding(16.dp)
+
+    ) {
+        Text(text = module.title, fontWeight = FontWeight.Bold, modifier = Modifier.padding(16.dp).fillMaxWidth())
+
     }
+
 }
 
 @Preview(showBackground = true)
